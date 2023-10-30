37b1becbe3b36133b7b843bb28f42b26501c97fe
ConnorBieszk <84150127+pyluaVR@users.noreply.github.com>
2023-10-30 18:59:40 -0500

    Add project files.

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace lumen
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.pyluadotcode.gorillatag.lumen";
+        public const string Name = "lumen";
+        public const string Version = "1.0.0";
+    }
+}
